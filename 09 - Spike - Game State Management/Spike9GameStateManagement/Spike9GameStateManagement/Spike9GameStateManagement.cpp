0c334de8a43d71415d8a6df47802ed435742e56b
huf0001 <samhuffer01@gmail.com>
2019-08-27 13:35:15 +1000

    More progress with performance management, diagrams for state management, blank project for state management, instruction pdfs for a number of tasks.

---
+++ 09 - Spike - Game State Management/Spike9GameStateManagement/Spike9GameStateManagement/Spike9GameStateManagement.cpp
@@ -0,0 +1,10 @@
+// Spike9GameStateManagement.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include "pch.h"
+#include <iostream>
+
+int main()
+{
+   
+}
